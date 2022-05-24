81bbcf49ff88c6ce68d859f418b24c7f83a5b039
chinki kumari singh <singhchinkikumari@gmail.com>
2022-05-24 15:43:18 +0530

    [chinki] Employee Status

---
+++ EmployeWageCom/EmployeWageCom/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+EmployeWageCom.EmployeeAtendence employeeAtendence = new EmployeWageCom.EmployeeAtendence();
+employeeAtendence.Attendence();
