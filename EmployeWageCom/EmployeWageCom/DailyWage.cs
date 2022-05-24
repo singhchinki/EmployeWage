0ee8ce61409c02a7b2ccbff6cb390dcb9095e1a0
chinki kumari singh <singhchinkikumari@gmail.com>
2022-05-24 15:59:44 +0530

    [chinki] Employee Dailywage

---
+++ EmployeWageCom/EmployeWageCom/DailyWage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeWageCom
+{
+    internal class DailyWage
+    {
+        public void Dailywage()
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+
+            //Predefined random class for generating random values
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            Console.WriteLine("Checking employee status" + empCheck);
+            
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+                empHrs = 0;
+            }
+            empWage = EMP_RATE_PER_HOUR * empHrs;
+            Console.WriteLine("Daily employee wage is" + empWage);
+            Console.ReadLine();
+        }
+    }
+}
--- EmployeWageCom/EmployeWageCom/Program.cs
+++ EmployeWageCom/EmployeWageCom/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 EmployeWageCom.EmployeeAtendence employeeAtendence = new EmployeWageCom.EmployeeAtendence();
 employeeAtendence.Attendence();
+EmployeWageCom.DailyWage dailyWage = new EmployeWageCom.DailyWage();
+dailyWage.Dailywage();
